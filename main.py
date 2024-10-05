04158fb8d709bac05b89662f3b488a192b9f5902
Dmytro Shvets <d.shvets.business@gmail.com>
2024-10-05 15:09:06 +0300

    added basic_test_df

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from basic_dfs.basic_df_Rechkalova import basic_test_df
+from basic_dfs.basic_df_Rechkalova import basic_test_df as basic_test_df2
+from basic_dfs.basic_df_Shvets import basic_test_df as basic_test_df3
 
-df = basic_test_df()
-df.show()
+df2 = basic_test_df2()
+df2.show()
+
+df3 = basic_test_df3()
+df3.show()
