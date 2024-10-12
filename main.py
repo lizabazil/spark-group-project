4e3409340f8ab94a8c90ea09d5309216d06d3bd6
Dmytro Shvets <d.shvets.business@gmail.com>
2024-10-12 14:57:12 +0300

    reading title_principals

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from basic_dfs.basic_df_Rechkalova import basic_test_df as basic_test_df2
 from basic_dfs.basic_df_Shvets import basic_test_df as basic_test_df3
 from basic_dfs.basic_df_Tretiak import basic_test_df as basic_test_df1
+from io_h import read_title_principals_df
+from setting import path
 
 df2 = basic_test_df2()
 df2.show()
@@ -10,3 +12,6 @@ df3.show()
 
 df1 = basic_test_df1()
 df1.show()
+
+df3_title_principals = read_title_principals_df(path)
+df3_title_principals.show()
