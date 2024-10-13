a369ed446ec3f20f1ade13333e627d2bfc1e7673
Yelyzaveta Tretiak <liza.tretjak@gmail.com>
2024-10-13 10:21:01 +0200

    deleted overwite mode

--- io_h.py
+++ io_h.py
@@ -10,12 +10,12 @@ def read_name_basics_df(path_to_df):
     :return: dataframe
     """
     spark = spark_session.getActiveSession()
-    name_basics_schema = t.StructType([t.StructField('number', t.StringType(), False),
-                                       t.StructField('name', t.StringType(), False),
-                                       t.StructField('birth_year', t.IntegerType(), True),
-                                       t.StructField('death_year', t.IntegerType(), True),
-                                       t.StructField('primary_profession', t.StringType(), True),
-                                       t.StructField('known_for_titles', t.StringType(), True), ])
+    name_basics_schema = t.StructType([t.StructField('nconst', t.StringType(), False),
+                                       t.StructField('primaryName', t.StringType(), False),
+                                       t.StructField('birthYear', t.IntegerType(), True),
+                                       t.StructField('deathYear', t.IntegerType(), True),
+                                       t.StructField('primaryProfession', t.StringType(), True),
+                                       t.StructField('knownForTitles', t.StringType(), True), ])
 
     df = spark.read.csv(path_to_df, sep=r'\t', header=True, nullValue='null', schema=name_basics_schema)
     return df
@@ -28,4 +28,5 @@ def write_name_basics_to_csv(df_name_basics, write_path_to_df=write_path):
     :param write_path_to_df:
     :return:
     """
+    df_name_basics.write.csv(write_path_to_df, header=True)
 
