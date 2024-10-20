093ff51c1776243efa1ded6bece1a9d9223065a9
Yelyzaveta Tretiak <liza.tretjak@gmail.com>
2024-10-20 20:27:39 +0200

    modified column primary_profession's type to array type

--- process_name_basics.py
+++ process_name_basics.py
@@ -1,4 +1,5 @@
 import re
+from pyspark.sql.functions import split
 
 
 def change_column_names_to_snake_case(name_basics_df):
@@ -30,3 +31,21 @@ def to_snake_case(name_column):
     """
     new_column_name = re.sub(r'(?<!^)(?=[A-Z])', '_', name_column)
     return new_column_name.lower()
+
+
+def make_primary_profession_col_array_type(name_basics_df):
+    """
+    To modify the column  'primary_profession' type to array type.
+
+    Args:
+        name_basics_df (pyspark dataframe): The name_basics dataframe.
+
+    Returns:
+        pyspark dataframe: dataframe with the modified column type 'primary_profession'.
+    """
+    primary_profession_col_name = 'primary_profession'
+    name_basics_df = name_basics_df.withColumn(primary_profession_col_name,
+                                               split(name_basics_df[primary_profession_col_name], ', '))
+    return name_basics_df
+
+
