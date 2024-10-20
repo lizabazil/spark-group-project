c78f6c8603f2cc5498377a610c9232d007e982b0
Yelyzaveta Tretiak <liza.tretjak@gmail.com>
2024-10-20 20:32:51 +0200

    modified column known_for_titles's type to array type

--- process_name_basics.py
+++ process_name_basics.py
@@ -45,7 +45,24 @@ def make_primary_profession_col_array_type(name_basics_df):
     """
     primary_profession_col_name = 'primary_profession'
     name_basics_df = name_basics_df.withColumn(primary_profession_col_name,
-                                               split(name_basics_df[primary_profession_col_name], ', '))
+                                               split(name_basics_df[primary_profession_col_name], ','))
     return name_basics_df
+
+
+def make_known_for_titles_col_array_type(name_basics_df):
+    """
+    To modify the column  'known_for_titles' type to array type.
+
+    Args:
+        name_basics_df (pyspark dataframe): The name_basics dataframe.
+
+    Returns:
+        pyspark dataframe: dataframe with the modified column type 'known_for_titles'.
+    """
+    known_for_titles_col_name = 'known_for_titles'
+    name_basics_df = name_basics_df.withColumn(known_for_titles_col_name,
+                                               split(name_basics_df[known_for_titles_col_name], ','))
+    return name_basics_df
+
 
 
