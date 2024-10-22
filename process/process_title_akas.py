1840b507e975d42a7fae8c1d93c3b1806058979f
Anna Rechkalova <ann.rechkalova@gmail.com>
2024-10-22 13:34:26 +0300

    created function that changes string column "types" to array type (make_types_col_array_type).

---
+++ process/process_title_akas.py
@@ -0,0 +1,24 @@
+from columns import types, attributes
+import pyspark.sql.functions as f
+
+
+def make_types_col_array_type(title_akas_df):
+    """
+    Change string column "types" to array type.
+    from developer.imdb.com:
+        "types (array) - Enumerated set of attributes for this alternative title.
+        One or more of the following: "alternative", "dvd", "festival", "tv", "video",
+        "working", "original", "imdbDisplay".
+        New values may be added in the future without warning"
+
+    Args:
+        title_akas_df (dataframe): The dataframe.
+
+    Returns:
+        dataframe: The dataframe with array values.
+    """
+    title_akas_df = title_akas_df.withColumn(types,
+                                             f.split(title_akas_df[types], ','))
+    return title_akas_df
+
+
