ba001211e89762c98b5f2e38307b18fb8f20a808
Yelyzaveta Tretiak <liza.tretjak@gmail.com>
2024-10-22 09:01:11 +0200

    deleted method with dropping col

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from basic_dfs.basic_df_Shvets import basic_test_df as basic_test_df3
 from basic_dfs.basic_df_Tretiak import basic_test_df as basic_test_df1
 from io_h import read_name_basics_df, write_name_basics_to_csv
 from io_h import read_title_principals_df, write_title_principals_df_to_csv, read_title_akas_df, write_title_akas_df_to_csv
-from setting import path
+from io_h import read_title_basics_df
+from setting import *
 from process.process_name_basics import (make_primary_profession_col_array_type,
                                          make_known_for_titles_col_array_type, create_age_col, create_is_alive_col,
                                          rename_nconst_col)
-from process.common_functions import change_column_names_to_snake_case
+from process.common_functions import *
+from process.process_title_basics import *
 
 
 df2 = basic_test_df2()
@@ -20,7 +22,7 @@ df1 = basic_test_df1()
 df1.show()
 
 
-df1_name_basics = read_name_basics_df(path)  # liza's
+df1_name_basics = read_name_basics_df(name_basics_path)  # liza's
 # df1_name_basics.show(truncate=False)  # remove before commit
 # write_name_basics_to_csv(df1_name_basics)
 # change column names to snake_case style
@@ -36,8 +38,23 @@ renamed_col_nconst_df = rename_nconst_col(with_is_living_col_df)
 # renamed_col_nconst_df.show(truncate=False)
 write_name_basics_to_csv(renamed_col_nconst_df)
 
-df_title_akas = read_title_akas_df(path)
+df_title_akas = read_title_akas_df(title_akas_path)
 write_title_akas_df_to_csv(df_title_akas)
 
-df3_title_principals = read_title_principals_df(path)
+df3_title_principals = read_title_principals_df(title_principals_path)
 write_title_principals_df_to_csv(df3_title_principals)
+
+
+# title.basics.tsv
+title_basics_df = read_title_basics_df(title_basics_path)
+title_basics_to_snake_case_df = change_column_names_to_snake_case(title_basics_df)
+title_basics_genres_col_modified_df = make_genres_array_type(title_basics_to_snake_case_df)
+
+convert_is_adult_col_to_bool_df = convert_is_adult_col_to_boolean_type(title_basics_genres_col_modified_df)
+null_from_string_to_none_df = null_from_string_to_none(convert_is_adult_col_to_bool_df)
+# null_from_string_to_none_df.show(truncate=False)
+
+# write to the file
+write_name_basics_to_csv(null_from_string_to_none_df, title_basics_write_path)
+
+
--- process/process_title_basics.py
+++ process/process_title_basics.py
@@ -16,20 +16,6 @@ def make_genres_array_type(df):
     return df
 
 
-def drop_end_year_col(title_basics_df):
-    """
-    To delete column 'end_year' from the dataframe, as there are too many null values
-    in this column (more than 70% of rows in dataframe).
-
-    Args:
-        title_basics_df: dataframe
-
-    Returns:
-        (pyspark dataframe): modified dataframe
-    """
-    return title_basics_df.drop(end_year)
-
-
 def convert_is_adult_col_to_boolean_type(title_basics_df):
     """
     Convert column 'is_adult' type to boolean type.
