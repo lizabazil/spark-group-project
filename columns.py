cae0d9b0e6efe51f31e8528fdbf30b116adec0e1
Anna Rechkalova <ann.rechkalova@gmail.com>
2024-10-22 13:12:04 +0300

    added columns for title.akas and title.episode

--- columns.py
+++ columns.py
@@ -8,3 +8,19 @@ primary_profession = 'primary_profession'
 known_for_titles = 'known_for_titles'
 age = 'age'
 is_alive = 'is_alive'
+
+# column names from 'title_akas' dataframe
+title_id = "title_id"
+ordering = "ordering"
+title = "title"
+region = "region"
+language = "language"
+types = "types"
+attributes = "attributes"
+is_original_title = "is_original_title"
+
+# column names from 'title_episode' dataframe
+tconst = "tconst"
+parent_tconst = "parent_tconst"
+season_number = "season_number"
+episode_number = "episode_number"
