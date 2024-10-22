4bef7a88eb40791bfae9c9d5cc5c24e8387c6417
Anna Rechkalova <ann.rechkalova@gmail.com>
2024-10-22 14:08:07 +0300

    added "process_title_episode" for future functions

--- main.py
+++ main.py
@@ -13,31 +13,31 @@ from process.process_title_akas import (make_types_col_array_type, make_attribut
                                         make_is_original_title_col_boolean_type)
 
 
-# df2 = basic_test_df2()
-# df2.show()
-#
-# df3 = basic_test_df3()
-# df3.show()
-#
-# df1 = basic_test_df1()
-# df1.show()
+df2 = basic_test_df2()
+df2.show()
 
-#
-# df1_name_basics = read_name_basics_df(path)  # liza's
-# # df1_name_basics.show(truncate=False)  # remove before commit
-# # write_name_basics_to_csv(df1_name_basics)
-# # change column names to snake_case style
-# snake_case_name_basics_df = change_column_names_to_snake_case(df1_name_basics)
-# # change primary_profession type to array type
-# modified_col_primary_profession_df = make_primary_profession_col_array_type(snake_case_name_basics_df)
-# modified_col_known_for_titles_df = make_known_for_titles_col_array_type(modified_col_primary_profession_df)
-#
-# # create new column 'age'
-# with_age_col_df = create_age_col(modified_col_known_for_titles_df)
-# with_is_living_col_df = create_is_alive_col(with_age_col_df)
-# renamed_col_nconst_df = rename_nconst_col(with_is_living_col_df)
-# # renamed_col_nconst_df.show(truncate=False)
-# write_name_basics_to_csv(renamed_col_nconst_df)
+df3 = basic_test_df3()
+df3.show()
+
+df1 = basic_test_df1()
+df1.show()
+
+
+df1_name_basics = read_name_basics_df(path)  # liza's
+# df1_name_basics.show(truncate=False)  # remove before commit
+# write_name_basics_to_csv(df1_name_basics)
+# change column names to snake_case style
+snake_case_name_basics_df = change_column_names_to_snake_case(df1_name_basics)
+# change primary_profession type to array type
+modified_col_primary_profession_df = make_primary_profession_col_array_type(snake_case_name_basics_df)
+modified_col_known_for_titles_df = make_known_for_titles_col_array_type(modified_col_primary_profession_df)
+
+# create new column 'age'
+with_age_col_df = create_age_col(modified_col_known_for_titles_df)
+with_is_living_col_df = create_is_alive_col(with_age_col_df)
+renamed_col_nconst_df = rename_nconst_col(with_is_living_col_df)
+# renamed_col_nconst_df.show(truncate=False)
+write_name_basics_to_csv(renamed_col_nconst_df)
 
 df_title_akas = read_title_akas_df(path)
 df_snake_case_akas = change_column_names_to_snake_case(df_title_akas)
@@ -48,11 +48,11 @@ df_title_akas_is_original_title_boolean = make_is_original_title_col_boolean_typ
 # df_title_akas_is_original_title_boolean.show()
 write_title_akas_df_to_csv(df_title_akas_is_original_title_boolean)
 
-# df_episode = read_title_episode_df(path)
-# df_snake_case_episode = change_column_names_to_snake_case(df_episode)
-# df_title_episode_without_n = null_from_string_to_none(df_snake_case_episode)
-# # df_title_episode_without_n.show()
-# write_title_episode_df_to_csv(df_title_episode_without_n)
-#
-# df3_title_principals = read_title_principals_df(path)
-# write_title_principals_df_to_csv(df3_title_principals)
+df_episode = read_title_episode_df(path)
+df_snake_case_episode = change_column_names_to_snake_case(df_episode)
+df_title_episode_without_n = null_from_string_to_none(df_snake_case_episode)
+# df_title_episode_without_n.show()
+write_title_episode_df_to_csv(df_title_episode_without_n)
+
+df3_title_principals = read_title_principals_df(path)
+write_title_principals_df_to_csv(df3_title_principals)
