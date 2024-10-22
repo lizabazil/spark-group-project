c1f5cd6794c58e7fbde47ec980a5f9c9855c8857
Anna Rechkalova <ann.rechkalova@gmail.com>
2024-10-22 13:46:35 +0300

    created function that changes string column "attributes" to array type (make_attribute_col_array_type).

--- process/process_title_akas.py
+++ process/process_title_akas.py
@@ -22,3 +22,18 @@ def make_types_col_array_type(title_akas_df):
     return title_akas_df
 
 
+def make_attribute_col_array_type(title_akas_df):
+    """
+    Change string column "attribute" to array type.
+    from developer.imdb.com:
+        "attributes (array) - Additional terms to describe this alternative title, not enumerated"
+
+    Args:
+        title_akas_df (dataframe): The dataframe.
+
+    Returns:
+        dataframe: The dataframe with array values.
+    """
+    title_akas_df = title_akas_df.withColumn(attributes,
+                                             f.split(title_akas_df[attributes], ','))
+    return title_akas_df
