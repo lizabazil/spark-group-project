716ff642cd0ce3cb2be72045e7c011cf980b778a
Yelyzaveta Tretiak <liza.tretjak@gmail.com>
2024-10-26 16:56:19 +0200

    added method for filling column end_year (title.basics df) with the value of column start_year

--- cleaning.py
+++ cleaning.py
@@ -1,4 +1,5 @@
 from columns import *
+import pyspark.sql.functions as f
 
 
 def drop_col_birth_year_from_name_basics_df(df):
@@ -23,3 +24,17 @@ def drop_col_death_year_from_name_basics_df(df):
         (pyspark dataframe): modified dataframe name.basics with dropped column death_year
     """
     return df.drop(death_year)
+
+
+def fill_col_end_year_in_title_basics_df(df):
+    """
+    Fill column end_year with values from column start_year in title.basics dataframe.
+
+    Args:
+        df (pyspark dataframe): dataframe title.basics
+
+    Returns:
+        (pyspark dataframe): modified dataframe title.basics with filled column end_year
+    """
+    return df.withColumn(end_year, f.when(f.col(end_year).isNull() & ~f.col(start_year).isNull(), f.col(start_year))
+                         .otherwise(f.col(end_year)))
