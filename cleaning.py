3128489cdb23fa026c596f8f93795209846209e8
Yelyzaveta Tretiak <liza.tretjak@gmail.com>
2024-10-26 16:18:20 +0200

    created method for dropping column death_year from df name.basics

--- cleaning.py
+++ cleaning.py
@@ -10,3 +10,16 @@ def drop_col_birth_year_from_name_basics_df(df):
         (pyspark dataframe): modified dataframe name.basics with dropped column birth_year
     """
     return df.drop(birth_year)
+
+
+def drop_col_death_year_from_name_basics_df(df):
+    """
+    Drop column death_year from dataframe name.basics
+
+    Args:
+         df (pyspark dataframe): dataframe name.basics
+
+    Returns:
+        (pyspark dataframe): modified dataframe name.basics with dropped column death_year
+    """
+    return df.drop(death_year)
