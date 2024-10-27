748599476c20ccd4c3602fcfcba3797b67a7c72a
Anna Rechkalova <ann.rechkalova@gmail.com>
2024-10-27 19:37:12 +0200

    created function 16. People who are known for only one title of movie.

--- filtering/filtering_Rechkalova.py
+++ filtering/filtering_Rechkalova.py
@@ -1,6 +1,5 @@
-# 16. People who are known for only one title of movie.
 # 17. Which titles have Ukrainian translations.
-from columns import primary_profession
+from columns import primary_profession, known_for_titles
 import pyspark.sql.functions as f
 
 
@@ -9,4 +8,10 @@ def actors_or_actresses_and_directors_at_the_same_time(df):
     df = df.filter((f.array_contains(f.col(primary_profession), "actor") |
                     f.array_contains(f.col(primary_profession), "actress")) &
                    f.array_contains(f.col(primary_profession), "director"))
+    return df
+
+
+# 16. People who are known for only one title of movie.
+def people_who_are_known_for_one_title_movie(df):
+    df = df.filter(f.size(f.col(known_for_titles)) == 1)
     return df
