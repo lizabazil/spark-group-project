4dccdcfe40a9adabb5160bb2d58e6acfc42b5d29
Anna Rechkalova <ann.rechkalova@gmail.com>
2024-10-27 19:52:32 +0200

    created function 17. Which titles have Ukrainian translations (regions and language columns).
    + dockstrings for prev func

--- filtering/filtering_Rechkalova.py
+++ filtering/filtering_Rechkalova.py
@@ -1,5 +1,4 @@
-# 17. Which titles have Ukrainian translations.
-from columns import primary_profession, known_for_titles
+from columns import primary_profession, known_for_titles, region, language
 import pyspark.sql.functions as f
 
 
@@ -14,4 +13,10 @@ def actors_or_actresses_and_directors_at_the_same_time(df):
 # 16. People who are known for only one title of movie.
 def people_who_are_known_for_one_title_movie(df):
     df = df.filter(f.size(f.col(known_for_titles)) == 1)
+    return df
+
+
+# 17. Which titles have Ukrainian translations (regions and language columns).
+def titles_with_ukrainian_translation(df):
+    df = df.filter((f.col(region) == "UA") | (f.col(language) == "uk"))
     return df
