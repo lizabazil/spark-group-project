697d8dad7a98e5f1fc437f4699e275b948408a14
Dmytro Shvets <d.shvets.business@gmail.com>
2024-10-27 14:46:38 +0200

    dropped null rows in title_crew df

--- cleaning.py
+++ cleaning.py
@@ -1,4 +1,5 @@
-from columns import job, characters
+from columns import job, characters, directors, writers
+import pyspark.sql.functions as f
 
 
 def drop_job_column_in_title_principals(df):
@@ -23,3 +24,15 @@ def drop_characters_column_in_title_principals(df):
     """
     df_without_characters_column = df.drop(characters)
     return df_without_characters_column
+
+
+def drop_null_rows_in_title_crew(df):
+    """
+    Drops rows in title_crew df where both values (directors and writers) are null because they are useless
+    Args:
+        df: Dataframe title_crew.
+    Returns:
+        df: The title_crew dataframe without null rows.
+    """
+    df_without_null_rows = df.filter(f.col(directors).isNotNull() | f.col(writers).isNotNull())
+    return df_without_null_rows
