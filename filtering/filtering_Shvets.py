c098389698328aa28ea4e43335fcecad6f73f893
Dmytro Shvets <d.shvets.business@gmail.com>
2024-10-28 19:07:23 +0200

    added function for question 20

--- filtering/filtering_Shvets.py
+++ filtering/filtering_Shvets.py
@@ -1,6 +1,6 @@
 import pyspark.sql.functions as f
 
-from columns import primary_profession
+from columns import primary_profession, genres, runtime_minutes
 
 
 def get_directors_not_producers(df):
@@ -25,4 +25,17 @@ def get_people_with_only_2_professions(df):
         df: people with only top-2 professions
     """
     filtered_df = df.filter(f.array_size(f.col(primary_profession)) == 2)
+    return filtered_df
+
+
+def get_dramas_with_more_than_70_mins_runtime(df):
+    """
+    20. Which movies have drama genre and runtime minutes more than 70.
+    Args:
+        df: title_basics dataframe
+    Returns:
+        df: movies with drama genre and runtime minutes more than 70
+    """
+    filtered_df = df.filter((f.col(runtime_minutes) > 70)
+                            & f.array_contains(f.col(genres), 'Drama'))
     return filtered_df
