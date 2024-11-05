111e06811be824ad51f1196a6d0ecfa9c6e5f60c
Dmytro Shvets <d.shvets.business@gmail.com>
2024-11-05 16:30:59 +0200

    added function for business question 23

--- business_questions/agg_sort_window_Shvets.py
+++ business_questions/agg_sort_window_Shvets.py
@@ -1,6 +1,6 @@
 import pyspark.sql.functions as f
-
-from columns import primary_profession, id_person, genres, runtime_minutes, start_year
+from pyspark.sql import Window
+from columns import primary_profession, id_person, genres, runtime_minutes, start_year, original_title
 
 
 def top_10_professions_by_number_of_people(name_basics):
@@ -46,7 +46,7 @@ def animated_fantasy_films_count_per_decade(title_basics):
     Args:
         title_basics: The title_basics dataframe.
     Returns:
-        average_runtime_per_genre_df: dataframe with two columns: decade and movies_number
+        animated_fantasy_decades: dataframe with two columns: decade and movies_number
     """
     decade = 'decade'
     animated_fantasy_decades = (title_basics
@@ -59,3 +59,23 @@ def animated_fantasy_films_count_per_decade(title_basics):
                                 .withColumnRenamed('count', 'movies_number')
                                 .orderBy(decade, ascending=True))
     return animated_fantasy_decades
+
+
+def top_3_long_runtime_titles_per_decade(title_basics):
+    """
+    23. Top 3 titles with the longest runtime for each decade
+    Args:
+        title_basics: The title_basics dataframe.
+    Returns:
+        top_long_runtime_titles: dataframe with 4 columns: decade, original_title, runtime_minutes, rank
+    """
+    decade = 'decade'
+    rank = 'rank'
+    decade_runtime_window = Window.partitionBy(decade).orderBy(f.desc(runtime_minutes))
+    top_long_runtime_titles = (title_basics
+                               .withColumn(decade,
+                                           (f.col(start_year) - (f.col(start_year) % 10)))
+                               .withColumn(rank, f.row_number().over(decade_runtime_window))
+                               .select(decade, original_title, runtime_minutes, rank)
+                               .filter((f.col(rank) <= 3)))
+    return top_long_runtime_titles
