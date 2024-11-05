40d3d28fffc7bad5e65d3cc6b778b907e25d44da
Dmytro Shvets <d.shvets.business@gmail.com>
2024-11-05 16:14:48 +0200

    added function for business question 21

--- business_questions/agg_sort_window_Shvets.py
+++ business_questions/agg_sort_window_Shvets.py
@@ -1,6 +1,6 @@
 import pyspark.sql.functions as f
 
-from columns import primary_profession, id_person
+from columns import primary_profession, id_person, genres, runtime_minutes
 
 
 def top_10_professions_by_number_of_people(name_basics):
@@ -20,3 +20,21 @@ def top_10_professions_by_number_of_people(name_basics):
                           .orderBy(people_number, ascending=False)
                           .limit(10))
     return top_10_professions
+
+
+def average_runtime_per_genre(title_basics):
+    """
+    21. What is the average runtime for titles of each genre?
+    Args:
+        title_basics: The title_basics dataframe.
+    Returns:
+        average_runtime_per_genre_df: dataframe with two columns: genre and average_runtime
+    """
+    genre = 'genre'
+    average_runtime = 'average_runtime'
+    average_runtime_per_genre_df = (title_basics
+                                    .withColumn(genre, f.explode(genres))
+                                    .groupBy(genre)
+                                    .agg(f.avg(runtime_minutes).alias(average_runtime))
+                                    .orderBy(average_runtime, ascending=True))
+    return average_runtime_per_genre_df
