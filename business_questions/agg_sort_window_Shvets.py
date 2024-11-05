e31602762ec594adc1fd14afbe6faa74b251365e
Dmytro Shvets <d.shvets.business@gmail.com>
2024-11-05 16:23:36 +0200

    added function for business question 22

--- business_questions/agg_sort_window_Shvets.py
+++ business_questions/agg_sort_window_Shvets.py
@@ -1,6 +1,6 @@
 import pyspark.sql.functions as f
 
-from columns import primary_profession, id_person, genres, runtime_minutes
+from columns import primary_profession, id_person, genres, runtime_minutes, start_year
 
 
 def top_10_professions_by_number_of_people(name_basics):
@@ -38,3 +38,24 @@ def average_runtime_per_genre(title_basics):
                                     .agg(f.avg(runtime_minutes).alias(average_runtime))
                                     .orderBy(average_runtime, ascending=True))
     return average_runtime_per_genre_df
+
+
+def animated_fantasy_films_count_per_decade(title_basics):
+    """
+    22. How many animated fantasy films were released in total each decade?
+    Args:
+        title_basics: The title_basics dataframe.
+    Returns:
+        average_runtime_per_genre_df: dataframe with two columns: decade and movies_number
+    """
+    decade = 'decade'
+    animated_fantasy_decades = (title_basics
+                                .filter(f.array_contains(f.col(genres), 'Animation')
+                                        & f.array_contains(f.col(genres), 'Fantasy'))
+                                .withColumn(decade,
+                                            (f.col(start_year) - (f.col(start_year) % 10)))
+                                .groupBy(decade)
+                                .count()
+                                .withColumnRenamed('count', 'movies_number')
+                                .orderBy(decade, ascending=True))
+    return animated_fantasy_decades
